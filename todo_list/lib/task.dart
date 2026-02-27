bae33080127852df2f88bae03c8eab4b0688eb78
Tedros Teshome <tedrhanna@gmail.com>
2026-02-27 17:37:18 +0300

    feat: create edit page and edited task model for implementing priority

--- todo_list/lib/task.dart
+++ todo_list/lib/task.dart
@@ -1,9 +1,13 @@
+enum Priority { high, medium, low }
+
 class Task {
   String title;
   bool isDone;
+  Priority priority;
 
   Task({
     required this.title,
     this.isDone = false,
+    this.priority = Priority.medium,
   });
 }
