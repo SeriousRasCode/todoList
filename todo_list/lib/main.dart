fd947c6488c551305cbd9970b6fae68ef1503454
Tedros Teshome <tedrhanna@gmail.com>
2026-02-27 17:28:43 +0300

    feat: create files and refine main for use with route for navigation

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,116 +1,24 @@
-import  'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'add_task_page.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home:Home()));
+  runApp(const MyApp());
 }
 
-class Home extends StatefulWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  bool isChecked = false; 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      appBar:AppBar(
-        
-        title:Text('Todo List'),
-        actions: [
-          IconButton(onPressed: (){}, icon:Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon:Icon(Icons.nightlight_round)),
-        ],
-      ),
-
-      body:Column(
-        children:[
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text('Today', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-              ),
-              Expanded(child:Container(height: 10,)),
-              IconButton(onPressed: (){}, icon:Icon(Icons.calendar_today)),
-              IconButton(onPressed: (){}, icon:Icon(Icons.more_horiz)),
-            ],
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                        borderSide: BorderSide.none,
-                        ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      prefixIcon: Icon(Icons.add),
-                      hintText: 'Add a new task',
-                      contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: 10,),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView(
-                children: [
-                  LIstDesign('Buy groceries'),
-                  SizedBox(height: 10,),
-                  LIstDesign('Walk the dog'),
-                  SizedBox(height: 10,),
-                  LIstDesign('Finish Flutter project'),
-                  
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/add': (context) => const AddTaskPage(),
+      },
     );
-  }
-
-  Container LIstDesign(String text) {
-    return Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(8.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 1,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: ListTile(
-                    leading: Checkbox(
-                      value: isChecked, 
-                      onChanged: (bool? newValue) { 
-                        setState(() {
-                          isChecked = newValue ?? false; 
-                        });
-                      },
-                    ),
-                    title: Text(text),
-                    trailing: Icon(Icons.star_border),
-                  ),
-                );
   }
 }
