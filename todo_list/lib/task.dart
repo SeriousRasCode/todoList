9739ac5f352f69b2141021541f60c5e3d1f5989c
Tedros Teshome <tedrhanna@gmail.com>
2026-02-27 19:22:24 +0300

    feat: add description into task model

--- todo_list/lib/task.dart
+++ todo_list/lib/task.dart
@@ -2,11 +2,13 @@ enum Priority { high, medium, low }
 
 class Task {
   String title;
+  String description;
   bool isDone;
   Priority priority;
 
   Task({
     required this.title,
+    this.description = '',
     this.isDone = false,
     this.priority = Priority.medium,
   });
