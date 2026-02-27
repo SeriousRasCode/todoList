79896bdd8e7c1effe082ec39dc0e6e679d85ec70
Tedros Teshome <tedrhanna@gmail.com>
2026-02-27 17:38:04 +0300

    feat: add day and night mode theme with state

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'add_task_page.dart';
+import 'edit_task_page.dart';
+import 'task.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  static _MyAppState of(BuildContext context) =>
+      context.findAncestorStateOfType<_MyAppState>()!;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.light;
+
+  void toggleTheme() {
+    setState(() {
+      themeMode =
+          themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Todo App',
+      themeMode: themeMode,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(),
         '/add': (context) => const AddTaskPage(),
+        '/edit': (context) {
+          final task = ModalRoute.of(context)!.settings.arguments as Task;
+          return EditTaskPage(task: task);
+        },
       },
     );
   }
