5186ba36546ee2d41142180b15c29a3007a5edc9
Tedros Teshome <tedrhanna@gmail.com>
2026-02-27 17:40:29 +0300

    modify card for including editition

--- todo_list/lib/task_card.dart
+++ todo_list/lib/task_card.dart
@@ -5,45 +5,60 @@ class TaskCard extends StatelessWidget {
   final Task task;
   final Function(bool?) onChanged;
   final VoidCallback onDelete;
+  final VoidCallback onEdit;
 
   const TaskCard({
     super.key,
     required this.task,
     required this.onChanged,
     required this.onDelete,
+    required this.onEdit,
   });
+
+  Color getPriorityColor() {
+    switch (task.priority) {
+      case Priority.high:
+        return Colors.red;
+      case Priority.medium:
+        return Colors.orange;
+      case Priority.low:
+        return Colors.green;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 10),
       decoration: BoxDecoration(
-        color: Colors.white,
         borderRadius: BorderRadius.circular(8),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.4),
+            color: Colors.grey.withOpacity(0.3),
             blurRadius: 5,
             offset: const Offset(0, 3),
           ),
         ],
       ),
       child: ListTile(
-        leading: Checkbox(
-          value: task.isDone,
-          onChanged: onChanged,
-        ),
+        tileColor: Theme.of(context).cardColor,
+        leading: Checkbox(value: task.isDone, onChanged: onChanged),
         title: Text(
           task.title,
           style: TextStyle(
             decoration:
                 task.isDone ? TextDecoration.lineThrough : null,
-            color: task.isDone ? Colors.grey : Colors.black,
           ),
         ),
-        trailing: IconButton(
-          icon: const Icon(Icons.delete),
-          onPressed: onDelete,
+        subtitle: Text(
+          task.priority.name.toUpperCase(),
+          style: TextStyle(color: getPriorityColor()),
+        ),
+        trailing: Wrap(
+          children: [
+            IconButton(icon: const Icon(Icons.edit), onPressed: onEdit),
+            IconButton(icon: const Icon(Icons.delete), onPressed: onDelete),
+          ],
         ),
       ),
     );
